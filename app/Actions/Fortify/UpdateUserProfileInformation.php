ce16cfadb3a0b3f704928e03ac277d20469ea6af
Arthur Oliveira <arthurpmgb@gmail.com>
2021-10-29 14:04:47 -0300

    29-10

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -21,7 +21,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'usuario' => ['required', 'string', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'photo' => ['nullable', 'image', 'max:1024'],
+            'photo' => ['nullable', 'image'],
         ])->validateWithBag('updateProfileInformation');
 
         if (isset($input['photo'])) {
