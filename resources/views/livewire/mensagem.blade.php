2f48a28edbab834e022edc3be671c9838cd07624
Arthur Oliveira <arthurpmgb@gmail.com>
2022-04-01 20:22:35 -0300

    Update ml 01-04-2022 (2)

--- resources/views/livewire/mensagem.blade.php
+++ resources/views/livewire/mensagem.blade.php
@@ -83,13 +83,14 @@
               
             </div>
             
-            <div class="chat-input d-flex flex-row align-items-center" wire:ignore>
+            <div wire:key="inputchat" wire:ignore class="chat-input d-flex flex-row align-items-center">
 
-                <textarea class="input-msg mx-2" rows="1" autocomplete="off" placeholder="Mensagem" wire:model.defer="mensagem" wire:click.prevent="scrollDown"></textarea>
+                <textarea wire:key="txtarea" wire:ignore class="input-msg mx-2" rows="1" autocomplete="off" placeholder="Mensagem" wire:model.defer="mensagem" wire:click.prevent="scrollDown"></textarea>
 
                 <button class="btn btn-lg me-2 btn-send" wire:click.prevent="sendMessage()" wire:loading.attr="disabled">
                     <i class="fas fa-paper-plane"></i>
                 </button>
+
             </div>
 
         </div>
