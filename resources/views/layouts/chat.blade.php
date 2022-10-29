8ef42b0aced725dbab665d250ebfbbd95f965b57
Arthur Oliveira <arthurpmgb@gmail.com>
2022-10-29 17:08:39 -0300

    Update user scalable none

--- resources/views/layouts/chat.blade.php
+++ resources/views/layouts/chat.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
